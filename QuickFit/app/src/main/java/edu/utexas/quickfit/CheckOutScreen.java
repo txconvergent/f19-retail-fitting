aed3ab7dd450e16193c4f25a627a90caf8716475
Justin Lee <justinolee@utexas.edu>
2019-10-26 15:28:49 -0500

    Take in phone number to checkout Added final thank you page

--- QuickFit/app/src/main/java/edu/utexas/quickfit/CheckOutScreen.java
+++ QuickFit/app/src/main/java/edu/utexas/quickfit/CheckOutScreen.java
@@ -1,13 +1,30 @@
 package edu.utexas.quickfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckOutScreen extends AppCompatActivity {
+
+    private Button checkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out_screen);
+
+        checkout = (Button) findViewById(R.id.checkoutButton);
+        checkout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                openThankYou();
+            }
+        });
+    }
+
+    public void openThankYou(){
+        Intent intent = new Intent(this, ThankYou.class);
+        startActivity(intent);
     }
 }
---
+++ QuickFit/app/src/main/java/edu/utexas/quickfit/ThankYou.java
@@ -0,0 +1,13 @@
+package edu.utexas.quickfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThankYou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thank_you);
+    }
+}
