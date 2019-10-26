a2b56571c0484aa28bb6e92d45b991bd89d9e728
Ashray Desai <ashray.v.desai1@gmail.com>
2019-10-26 15:21:09 -0500

    Added first screen functionality - need to make this the screen that appears upon app launch

--- QuickFit/app/src/main/java/edu/utexas/quickfit/FirstScreen.java
+++ QuickFit/app/src/main/java/edu/utexas/quickfit/FirstScreen.java
@@ -1,13 +1,44 @@
 package edu.utexas.quickfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstScreen extends AppCompatActivity {
+
+
+    Button checkInBtn;
+    Button checkOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
+
+        checkInBtn = findViewById(R.id.checkInBtn);
+        checkOutBtn = findViewById(R.id.checkOutBtn);
+
+        checkInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class );
+                startActivity(startIntent);
+            }
+        });
+
+        checkOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), CheckOutScreen.class );
+                startActivity(startIntent);
+            }
+        });
+
+
+
+
+
     }
 }
