8ac50d35b03f61ae9c221210d1ed2261fb184707
Justin Lee <justinolee@utexas.edu>
2019-10-30 16:42:01 -0500

    Added code to send sms

--- QuickFit/app/src/main/java/edu/utexas/quickfit/MainActivity.java
+++ QuickFit/app/src/main/java/edu/utexas/quickfit/MainActivity.java
@@ -1,22 +1,36 @@
 package edu.utexas.quickfit;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int SEND_SMS_PERMISSION_REQUEST_CODE = 1;
+
+    EditText number;
+    String message;
+    Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button checkInButton = findViewById(R.id.checkinBtn);
+        final Button checkInButton = findViewById(R.id.checkMeInButton);
         final Switch phoneOrEmail = findViewById(R.id.phoneOrEmail);
 
         //Updates input boxes for user choice of phone number or email
@@ -24,28 +38,48 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     ((TextView)findViewById(R.id.phoneEmailTitle)).setText("Email Address:");
-                    ((TextView)findViewById(R.id.phoneEmailEditText)).setHint("johndoe@email.com");
+                    ((TextView)findViewById(R.id.inputNumber)).setHint("johndoe@email.com");
                 } else {
                     ((TextView)findViewById(R.id.phoneEmailTitle)).setText("Phone Number:");
-                    ((TextView)findViewById(R.id.phoneEmailEditText)).setHint("(XXX) XXX-XXXX");
+                    ((TextView)findViewById(R.id.inputNumber)).setHint("(XXX) XXX-XXXX");
                 }
             }
         });
 
+        final boolean messagePermission = checkPermission(Manifest.permission.SEND_SMS);
         //Runs check in queue process
         checkInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             //We will add send text message/email methods here
             public void onClick(View v){
-                if(phoneOrEmail.isChecked()){
-                    //send Email method
+//                if(phoneOrEmail.isChecked()){
+//                    //send Email method
+//                }
+//                if(!phoneOrEmail.isChecked()) {
+                    number = findViewById(R.id.inputNumber);
+                    String phoneNumber = number.toString();
+                    message = "Thank you for checking in";
+                    send = findViewById(R.id.checkMeInButton);
+                    send.setEnabled(true);
+                    try{
+                        if(messagePermission){
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+                        }
+                    } catch(Exception e){
+                        Toast.makeText(getApplicationContext(),
+                                "failed",
+                                Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+
+
                 }
-                if(phoneOrEmail.isChecked()) {
-                    //send phone message method
-                }
-            }
+//            }
         });
+
     }
+
 
     //Gets name inputted in name text box, if nothing is entered, returns "Customer"
     private String getName(){
@@ -57,10 +91,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String getUserInfo() {
-        EditText userInfoText = findViewById(R.id.phoneEmailEditText);
+        EditText userInfoText = findViewById(R.id.inputNumber);
         String userInfo = userInfoText.getText().toString();
         return userInfo;
     }
 
+    public boolean checkPermission(String permission){
+        int check = ContextCompat.checkSelfPermission(this, permission);
+        return (check == PackageManager.PERMISSION_GRANTED);
+    }
 }
 
