cae5189253a3bd87256a05c3c3b26572dd44f799
Soham Patel <54907192+patelsoham@users.noreply.github.com>
2019-11-16 13:39:54 -0600

    Merge branch 'master' of /Users/sohampatel/AndroidStudioProjects/f19-retail-fitting with conflicts.

--- QuickFit/app/src/main/java/edu/utexas/quickfit/MainActivity.java
+++ QuickFit/app/src/main/java/edu/utexas/quickfit/MainActivity.java
@@ -1,15 +1,12 @@
 package edu.utexas.quickfit;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -17,32 +14,21 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView placeTxt;
-    Set<String> contactInfoSet = new HashSet<>();
-    ArrayList<String> peopleOrderedList = new ArrayList<>();
-
-    int position = 0;
 
     EditText number;
     String message = "Thank you for checking in";
     String phoneNo;
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    private List<String> queue = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        placeTxt = findViewById(R.id.placeTxt);
-        number = findViewById(R.id.inputPhoneNumber);
 
         final Button checkInButton = findViewById(R.id.checkMeInButton);
         final Switch phoneOrEmail = findViewById(R.id.phoneOrEmail);
@@ -52,10 +38,10 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     ((TextView)findViewById(R.id.phoneEmailTitle)).setText("Email Address:");
-                    ((TextView)findViewById(R.id.inputPhoneNumber)).setHint("johndoe@email.com");
+                    ((TextView)findViewById(R.id.inputNumber)).setHint("johndoe@email.com");
                 } else {
                     ((TextView)findViewById(R.id.phoneEmailTitle)).setText("Phone Number:");
-                    ((TextView)findViewById(R.id.inputPhoneNumber)).setHint("(XXX) XXX-XXXX");
+                    ((TextView)findViewById(R.id.inputNumber)).setHint("(XXX) XXX-XXXX");
                 }
             }
         });
@@ -64,25 +50,13 @@ public class MainActivity extends AppCompatActivity {
         checkInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             //We will add send text message/email methods here
-            public void onClick(View v) {
-
-                String phoneNum =  number.getText().toString();
-                if (phoneOrEmail.isChecked() && contactInfoSet.add(phoneNum)) {
-                    sendMail();
-                }
-                if (!phoneOrEmail.isChecked() && contactInfoSet.add(phoneNum)) {
+            public void onClick(View v){
+           //     if(phoneOrEmail.isChecked()){
+           //         sendMail();
+           //     }
+           //     if(!phoneOrEmail.isChecked()) {
                     sendText();
-                }
-
-                position++;
-
-                if (contactInfoSet.add(phoneNum)) {
-                    peopleOrderedList.add(phoneNum);
-
-                    Toast.makeText(MainActivity.this, "You are number " + position + " in line!", Toast.LENGTH_LONG * 3).show();
-                } else{
-                    Toast.makeText(MainActivity.this, "You have already been checked in!", Toast.LENGTH_LONG * 3).show();
-                }
+           //    }
             }
         });
 
@@ -102,10 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void sendText(){
         //sets the phone number to send the message to
+        number = findViewById(R.id.inputNumber);
+        phoneNo = number.toString();
 
-        phoneNo = number.getText().toString();
-
-        // turn on permissions
+        //check if the device has SMS permissions enabled
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -113,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
                         new String[]{Manifest.permission.SEND_SMS},
                         MY_PERMISSIONS_REQUEST_SEND_SMS);
         }
-        // send text if permissions enabled
         else{
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNo, null, message, null, null);
@@ -126,10 +99,12 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_SEND_SMS: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
                     // sends the SMS with SMSManager API
                     SmsManager smsManager = SmsManager.getDefault();
                     smsManager.sendTextMessage(phoneNo, null, message, null, null);
                 } else {
+                    Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_LONG).show();
                 }
             }
         }
@@ -145,10 +120,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String getUserInfo() {
-        EditText userInfoText = findViewById(R.id.inputPhoneNumber);
+        EditText userInfoText = findViewById(R.id.inputNumber);
         String userInfo = userInfoText.getText().toString();
         return userInfo;
     }
+
+
 
 }
 
