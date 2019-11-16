53a56834d44f6a83adcc759a15862dcfdc7bcc01
Soham Patel <54907192+patelsoham@users.noreply.github.com>
2019-11-16 13:48:34 -0600

    SMS Receiver Added

--- QuickFit/app/src/main/java/edu/utexas/quickfit/SMSReceiver.java
+++ QuickFit/app/src/main/java/edu/utexas/quickfit/SMSReceiver.java
@@ -3,13 +3,37 @@ package edu.utexas.quickfit;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
 
 public class SMSReceiver extends BroadcastReceiver {
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (intent.getAction().equals(SMS_RECEIVED)) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                // get sms objects
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                if (pdus.length == 0) {
+                    return;
+                }
+                // large message might be broken into many
+                SmsMessage[] messages = new SmsMessage[pdus.length];
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    sb.append(messages[i].getMessageBody());
+                }
+                String sender = messages[0].getOriginatingAddress();
+                String message = sb.toString();
+                Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+                // prevent any other broadcast receivers from receiving broadcast
+                // abortBroadcast();
+            }
+        }
     }
 }
