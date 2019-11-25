48788c2f588a2b2ce1c31b510a4f7f77d93803af
samantha3pen <samtuapen@utexas.edu>
2019-11-25 17:51:55 -0600

    manually copied CustomerDB, Person, and CheckOutScreen files from CustomerDB_and_Person branch

--- QuickFit/app/src/main/java/edu/utexas/quickfit/CheckOutScreen.java
+++ QuickFit/app/src/main/java/edu/utexas/quickfit/CheckOutScreen.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CheckOutScreen extends AppCompatActivity {
 
     private Button checkout;
+    //private EditText Edit;
+    private TextView Text;
+    private CustomerDB customerDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +21,14 @@ public class CheckOutScreen extends AppCompatActivity {
         setContentView(R.layout.activity_check_out_screen);
 
         checkout = (Button) findViewById(R.id.checkoutButton);
+        customerDB = CustomerDB.getInstance();
         checkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String input  = ((EditText)findViewById(R.id.Room_Number_Input)).getText().toString();
+                // use 'input' to clear customer info from data structure used in check in.
+                customerDB.checkout(Integer.parseInt(input));
+
                 Intent startIntent = new Intent(getApplicationContext(), ThankYou.class );
                 startActivity(startIntent);
             }
--- QuickFit/app/src/main/java/edu/utexas/quickfit/CustomerDB.java
+++ QuickFit/app/src/main/java/edu/utexas/quickfit/CustomerDB.java
@@ -1,5 +1,48 @@
 package edu.utexas.quickfit;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class CustomerDB {
 
+    private static CustomerDB instance = null;
+    Queue<Person> line;
+    Person[] rooms;
+
+    public CustomerDB()
+    {
+        line = new LinkedList<>();
+        rooms = new Person[5]; // num of rooms NOT dynamic
+    }
+
+    public void checkin(Person p)
+    {
+        for(int i = 0; i < rooms.length; i++){
+            if(rooms[i] == null){
+                rooms[i] = p;
+                return;
+            }
+        }
+        line.add(p);
+    }
+
+    public void checkout(int roomNum)
+    {
+        if(!line.isEmpty()){
+            Person nextP = line.poll();
+            rooms[roomNum] = nextP;
+        }
+        else
+            rooms[roomNum] = null;
+    }
+
+    /* To share 'database' across multiple files. */
+    public static CustomerDB getInstance()
+    {
+        if(instance == null)
+            instance = new CustomerDB();
+        return instance;
+    }
 }
